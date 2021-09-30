c0364b1ac5cb3b9663fa80b814e54ef40d1f7c44
metalencoder <hellinxskyline@gmail.com>
2021-09-30 21:58:58 +0300

    feat (task 5) : add task 5

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,10 +2,13 @@
 #include<conio.h>
 #include<math.h>
 #include<sstream>
+#include <cstdlib>
+#include<ctime>
 using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
 	char ch;
 	int n, k, i = 0;
 	stringstream dd;
@@ -46,7 +49,6 @@ int main()
 
 	case'3':
 		cout << "Введите два целых числа n и k: ";
-		
 		cin >> n >> k;
 		dd << n;
 		while (i != k)
@@ -57,31 +59,75 @@ int main()
 		break;
 	case '4':
 		setlocale(LC_ALL, "rus");
-		int x1, x2, x3, y1, y2, y3; //X - размеры кирпича, Y - размеры отверстия
+		int x1, x2, x3, y1, y2, y3; 
 		cout << "Введите размеры отверстия : высота : "; cin >> y1;
 		cout << "Введите размеры отверстия : длинна : "; cin >> y2;
 		cout << "Введите размеры отверстия : ширина : "; cin >> y3;
 		cout << "Введите размеры параллелепипеда : высота : "; cin >> x1;
 		cout << "Введите размеры параллелепипеда : длинна : "; cin >> x2;
 		cout << "Введите размеры параллелепипеда : ширина : "; cin >> x3;
-		if (y1 == x1 || y1 < x1 || y1 > x1) //Совпадение по высоте
+		if (y1 == x1 || y1 < x1 || y1 > x1) 
 		{
 			if (y1 == x1)cout<< "Высоты совпали\n";
 			if (y1 < x1)cout<< "Размер отверстия по высоте меньше высоты параллелепипеда\n";
 			if (y1 > x1)cout<< "Размер отверстия по высоте больше высоты параллелепипеда\n";
 		}
-		if (y2 == x2 || y2 < x2 || y2 > x2) //Совпадение по длинне
+		if (y2 == x2 || y2 < x2 || y2 > x2) 
 		{
 			if (y2 == x2)cout<< "Длина совпала\n";
 			if (y2 < x2)cout<< "Размер отверстия по длине меньше длины параллелепипеда\n";
 			if (y2 > x2)cout<< "Размер отверстия по длине больше длины параллелепипеда\n";
 		}
-		if (y3 == x3 || y3 < x3 || y3 > x3) //Совпадение по ширине
+		if (y3 == x3 || y3 < x3 || y3 > x3) 
 		{
 			if (y3 == x3)cout<< "Ширина совпала\n";
 			if (y3 < x3)cout<< "Размер отверстия по ширине меньше ширины параллелепипеда\n";
 			if (y3 > x3)cout<< "Размер отверстия по ширине больше ширины параллелепипеда\n";
 		}
+		break;
+	case'5':
+		{
+			int ran1, ran2, ran3, ran4;
+			ran1 = rand() % 10;
+			ran2 = rand() % 10;
+			ran3 = rand() % 10;
+			ran4 = rand() % 10;
+			cout << "Сгенерирован код: " << ran1 << ran2 << ran3 << ran4;
+			cout << endl;
+			for (int i = 0; i < 9; i++)
+			{
+				if (i == ran4)
+
+				{
+					for (int j = 0; j < 9; j++)
+					{
+						if (j == ran3)
+						{
+							for (int k = 0; k < 9; k++)
+								if (k == ran2)
+								{
+									{
+										for (int v = 0; v < 9; v++)
+											if (v == ran1)
+											{
+												{
+
+													cout << "Код найден " << v << k << j << i;
+
+												}
+											}
+									}
+
+								}
+						}
+					}
+				}
+
+			}
+		}
+		break;
+	default:cout << "ошибка ввода" << endl;
+		
 	}
 
 	 
